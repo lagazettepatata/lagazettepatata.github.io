dc36e37940849fc4c033c57d6099e20623c7115c
Alexandre Ribeiro <alexmcaribe@Alexandres-MacBook-Pro.local>
2023-08-22 17:18:09 +0100

    add embla carousel and cover image

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,43 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import React, { useCallback } from 'react'
+import useEmblaCarousel from 'embla-carousel-react'
 
 export default function Home() {
+  const [emblaRef, emblaApi] = useEmblaCarousel()
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev()
+  }, [emblaApi])
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext()
+  }, [emblaApi])
+
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+    <div className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          <div className="embla__slide">
+            <div className="container">
+              <main>
+                <Image
+                  src="/cover.jpeg"
+                  layout="fill"
+                  // objectFit="cover"
+                  objectPosition="center"
+                  alt="La Gazette Patata"
+                />
+              </main>
+            </div>
+          </div>
+          <div className="embla__slide">Slide 2</div>
+          <div className="embla__slide">Slide 3</div>
+        </div>
+      </div>
     </div>
+
   )
 }
